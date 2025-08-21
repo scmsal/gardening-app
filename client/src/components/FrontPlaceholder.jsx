25200571d402fe5b81ae663012b2f2a947b8a590
scmsal <31stephcm@gmail.com>
2025-08-21 08:00:33 -0400

    modularized image for home page body

---
+++ client/src/components/FrontPlaceholder.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Image } from "react-bootstrap";
+import gardenImg from "../assets/garden-7028181_1280.jpg";
+
+function FrontPlaceholder() {
+  return (
+    <div className="mb-3 w-50">
+      <Image
+        src={gardenImg}
+        alt="vegetable garden with a wheelbarrow"
+        className="img-fluid"
+      />
+      <p className="text-center fs-6">
+        Image by{" "}
+        <a
+          href="https://pixabay.com/users/alison506-4668088/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7028181"
+          className="link-success"
+        >
+          Alison Innes
+        </a>{" "}
+        from{" "}
+        <a
+          href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=7028181"
+          className="link-success"
+        >
+          Pixabay
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default FrontPlaceholder;
