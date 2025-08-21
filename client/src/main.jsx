d14c699be7ad061f548e342e0e683c5600b60155
scmsal <31stephcm@gmail.com>
2025-08-21 07:57:01 -0400

    temporary switch from App to App1 for development

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App1 from "./App1.jsx";
 import { Provider } from "react-redux";
 import store from "./store.js";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,7 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <App1 />
     </Provider>
   </React.StrictMode>
 );
