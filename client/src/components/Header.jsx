fbac9468fea0b4977866a4c1fe976c33c370fe1d
scmsal <31stephcm@gmail.com>
2025-08-21 07:50:29 -0400

    added new image to replace GIF; added navigation links

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,30 +1,49 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, Navbar, Image } from "react-bootstrap";
 import React, { useEffect } from "react";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons/faX";
 
 const Header = () => {
   return (
     <header>
       <Navbar
-        bg="success"
         variant="light"
         expand="lg"
         collapseOnSelect
         sticky="top"
+        className="d-flex bg-success justify-content-between px-4"
       >
-        <Container className="ms-0">
-          <div className="d-flex align-items-center">
-            <iframe
-              src="https://giphy.com/embed/EGzDfFUauCo6nDgfpk"
-              width="100"
-              height="70"
-              className="giphy-embed ms-1"
-              allowFullScreen
-            ></iframe>
-            <h1 className="text-white ms-3 mb-0 display-3">
-              What Can I Grow Now?
-            </h1>
+        <div className="d-flex align-items-center">
+          <div className="w-10">
+            {/* <iframe
+            src={"https://giphy.com/embed/EGzDfFUauCo6nDgfpk"}
+            width="100"
+            height="70"
+            className="giphy-embed ms-1"
+            allowFullScreen
+          ></iframe> */}
+            <Image
+              src={"/vegetables.jpg" || ""}
+              style={{ width: "5rem", height: "auto" }}
+            />
           </div>
-        </Container>
+
+          <h1 className="text-white ms-3 mb-0">What Can I Grow Now?</h1>
+        </div>
+        <div className="d-flex gap-4 align-items-center">
+          <NavLink to="/" className="custom-nav-link">
+            Home
+          </NavLink>
+          <NavLink to="/about" className="custom-nav-link">
+            About
+          </NavLink>
+          <NavLink to="/resources" className="custom-nav-link">
+            Resources
+          </NavLink>
+        </div>
       </Navbar>
     </header>
   );
