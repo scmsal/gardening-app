44c849fe83ab6a0170f34e47bb84c9d783c44d3d
scmsal <31stephcm@gmail.com>
2025-08-24 15:48:03 -0400

    removed unneeded code attributions

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,14 +3,14 @@ import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <footer className="bg-success bg-opacity-10 py-3 w-100 footer-bottom">
+    <footer className="bg-success bg-opacity-10 py-3 w-100 mt-auto">
       <Container fluid className="ms-3">
         <Row>
           <Col>Copyright &copy; 2025</Col>
           {/* <Col>
             <APICallCounter />
           </Col> */}
-          <Col className="text-center">
+          {/* <Col className="text-center">
             Plant Data: {"  "}
             <a
               href="https://perenual.com"
@@ -20,29 +20,15 @@ const Footer = () => {
             >
               Perenual.com
             </a>
-          </Col>
-          <Col className="text-end">
-            Plant hardiness zones: {""}
-            <a
-              href="https://rapidapi.com/fireside-worldwide-fireside-worldwide-default/api/plant-hardiness-zone"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              RapidAPI
-            </a>
-          </Col>
-          <Col className="text-center">
+          </Col> */}
+
+          {/* <Col className="text-center">
             GIF via{"  "}
             <a href="https://giphy.com/gifs/tomato-tomates-potager-EGzDfFUauCo6nDgfpk">
               GIPHY
             </a>
-          </Col>
-          <Col className="text-center">
-            Favicon by{" "}
-            <a href="https://www.freepik.com/free-vector/tomato-vegetable-organic-icon-isolated_169501930.htm#fromView=keyword&page=1&position=1&uuid=32966af5-595b-423f-b9b9-d08ce1490077&query=Tomato+Svg">
-              Freepik
-            </a>
-          </Col>
+          </Col> */}
+          <Col className="text-center"></Col>
         </Row>
       </Container>
     </footer>
