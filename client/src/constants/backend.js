ec89b8cde20e0d39fc696eef823ffcf5287bec6c
scmsal <31stephcm@gmail.com>
2025-08-26 10:45:47 -0400

    debugging backend URL connetion issues

--- client/src/constants/backend.js
+++ client/src/constants/backend.js
@@ -1,6 +1,17 @@
 // Backend API URL
+import.meta.env.REACT_APP_BACKEND_URL;
 
-const BACKEND_URL_ENDPOINT =
-  REACT_APP_BACKEND_URL || "http://localhost:5050/api";
+// === hardcoded option ===
+// const LOCAL_BACKEND_URL = "http://localhost:5050/api";
+
+// const PRODUCTION_BACKEND_URL = "https://gardening-app-hurn.onrender.com/api";
+
+// const BACKEND_URL_ENDPOINT =
+//   window.location.localhost === "localhost"
+//     ? LOCAL_BACKEND_URL
+//     : PRODUCTION_BACKEND_URL;
+
+//=== with env variables ===
+const BACKEND_URL_ENDPOINT = import.meta.env.REACT_APP_BACKEND_URL;
 
 export { BACKEND_URL_ENDPOINT };
