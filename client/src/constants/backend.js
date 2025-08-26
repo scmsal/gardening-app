34eb4b3a8e0f6b3200708ed37a84a95d16eda43f
scmsal <31stephcm@gmail.com>
2025-08-26 09:56:38 -0400

    edited BACKEND_URL_ENDPOINT

--- client/src/constants/backend.js
+++ client/src/constants/backend.js
@@ -1,7 +1,6 @@
 // Backend API URL
 
 const BACKEND_URL_ENDPOINT =
-  process.env.NODE_ENV === "production"
-    ? `${process.env.REACT_APP_BACKEND_URL}/api`
-    : "http://localhost:5050/api";
+  REACT_APP_BACKEND_URL || "http://localhost:5050/api";
+
 export { BACKEND_URL_ENDPOINT };
