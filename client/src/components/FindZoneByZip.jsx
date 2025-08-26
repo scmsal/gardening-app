0c73a622cd9edaddd2eb652e8e3698abfbb4b308
scmsal <31stephcm@gmail.com>
2025-08-26 12:00:52 -0400

    replaced my own component code with third party API widget because the original API server is unreliable.

--- client/src/components/FindZoneByZip.jsx
+++ client/src/components/FindZoneByZip.jsx
@@ -14,47 +14,61 @@ const FindZoneByZip = () => {
     dispatch(fetchHardinessZone(zipCode));
   };
 
+  //plant hardiness zone API widget at https://www.plantmaps.com/hardiness-zone-zipcode-search-widget.php
+
   return (
-    <div className="w-100">
-      <h4 className="text-success">Plant Hardiness Zone</h4>
-      <SearchBar
-        onSearch={handleSearch}
-        placeholder="Enter zip code"
-        label="Find my zone:"
-      />
-      <div className="text-end small">
-        <span>Powered by </span>{" "}
-        <a
-          href="https://rapidapi.com/fireside-worldwide-fireside-worldwide-default/api/plant-hardiness-zone"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="link-success"
-        >
-          Plant Hardiness Zone API
-        </a>
-      </div>
-      {loading && <Spinner as="span" animation="border" size="sm" />}
-      {zone && (
-        <div className="mt-3">
-          <p className="ms-3">
-            The hardiness zone for zip {zipInput} is <b>{zone}</b>
-          </p>
-        </div>
-      )}
-      {error && (
-        <Alert variant="danger" className="mt-3">
-          Error: {error}
-        </Alert>
-      )}
-    </div>
+    <iframe
+      src="https://www.plantmaps.com/hardiness-zone-zipcode.php"
+      name="Find the USDA Hardiness Zone for your Zipocde and Map it"
+      width="210"
+      scrolling="No"
+      frameborder="0"
+      marginheight="0"
+      marginwidth="0"
+    >
+      [Your browser doesn't support IFrames.{" "}
+      <a href="//www.plantmaps.com/hardiness-zone-zipcode.php" target="_blank">
+        Click here
+      </a>{" "}
+      to find the USDA hardiness zone for your zipcode.]
+    </iframe>
   );
+
+  //=== I have commented this out because the API server is unreliable
+  // return (
+  //   <div className="w-100">
+  //     <h4 className="text-success">Plant Hardiness Zone</h4>
+  //     <SearchBar
+  //       onSearch={handleSearch}
+  //       placeholder="Enter zip code"
+  //       label="Find my zone:"
+  //     />
+  //     <div className="text-end small">
+  //       <span>Powered by </span>{" "}
+  //       <a
+  //         href="https://rapidapi.com/fireside-worldwide-fireside-worldwide-default/api/plant-hardiness-zone"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //         className="link-success"
+  //       >
+  //         Plant Hardiness Zone API
+  //       </a>
+  //     </div>
+  //     {loading && <Spinner as="span" animation="border" size="sm" />}
+  //     {zone && (
+  //       <div className="mt-3">
+  //         <p className="ms-3">
+  //           The hardiness zone for zip {zipInput} is <b>{zone}</b>
+  //         </p>
+  //       </div>
+  //     )}
+  //     {error && (
+  //       <Alert variant="danger" className="mt-3">
+  //         Error: {error}
+  //       </Alert>
+  //     )}
+  //   </div>
+  // );
 };
 
 export default FindZoneByZip;
-
-/*
-Alternative plant hardiness zone API widget at https://www.plantmaps.com/hardiness-zone-zipcode-search-widget.php
-
-<iframe src="https://www.plantmaps.com/hardiness-zone-zipcode.php" name="Find the USDA Hardiness Zone for your Zipocde and Map it" width="210" scrolling="No" frameborder="0" marginheight="0" marginwidth="0">[Your browser doesn't support IFrames. <a href="//www.plantmaps.com/hardiness-zone-zipcode.php" target="_blank">Click here</a> to find the USDA hardiness zone for your zipcode.]</iframe>
-
-*/
