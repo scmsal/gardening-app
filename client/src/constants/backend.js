a51e4c2eb08c3f36bbd5b65c520306ede0110b21
scmsal <31stephcm@gmail.com>
2025-08-26 09:40:34 -0400

    edited to add REACT_APP_BACKEND_URL for production backend URL

--- client/src/constants/backend.js
+++ client/src/constants/backend.js
@@ -1,6 +1,7 @@
 // Backend API URL
+
 const BACKEND_URL_ENDPOINT =
   process.env.NODE_ENV === "production"
-    ? "https://gardening-app-hurn.onrender.com"
+    ? REACT_APP_BACKEND_URL
     : "http://localhost:5050/api";
 export { BACKEND_URL_ENDPOINT };
