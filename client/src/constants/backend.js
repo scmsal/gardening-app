b0041183e8fd3a553f4fc0ea20443258aef2734c
scmsal <31stephcm@gmail.com>
2025-08-26 09:17:51 -0400

    added Render.com deployment URL option

--- client/src/constants/backend.js
+++ client/src/constants/backend.js
@@ -1,3 +1,8 @@
-const BACKEND_URL_ENDPOINT = "http://localhost:5050/api";
+const NODE_ENV = "production";
+
+const BACKEND_URL_ENDPOINT =
+  NODE_ENV === "production"
+    ? "https://gardening-app-hurn.onrender.com/"
+    : "http://localhost:5050/api";
 
 export { BACKEND_URL_ENDPOINT };
