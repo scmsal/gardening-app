44b0a0b1954955d8c19039527ba114acd1413d23
scmsal <31stephcm@gmail.com>
2025-08-26 09:30:09 -0400

    edited api url

--- client/src/constants/backend.js
+++ client/src/constants/backend.js
@@ -1,7 +1,6 @@
 // Backend API URL
 const BACKEND_URL_ENDPOINT =
   process.env.NODE_ENV === "production"
-    ? "https://gardening-app-hurn.onrender.com/"
+    ? "https://gardening-app-hurn.onrender.com"
     : "http://localhost:5050/api";
-
 export { BACKEND_URL_ENDPOINT };
