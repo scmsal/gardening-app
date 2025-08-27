2b1153d4f88cd3a7095e70a9f7f812784fa8f933
scmsal <31stephcm@gmail.com>
2025-08-27 12:24:33 -0400

    added collapsible navbar for mobile view; adjusted font sizes for brand and links

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,48 +1,47 @@
 import { Container, Nav, Navbar, Image } from "react-bootstrap";
-import React, { useEffect } from "react";
-import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faX } from "@fortawesome/free-solid-svg-icons/faX";
 
 const Header = () => {
   return (
-    <header>
-      <Navbar
-        variant="light"
-        expand="lg"
-        collapseOnSelect
-        sticky="top"
-        className="d-flex bg-success justify-content-between px-4"
-      >
-        <div className="d-flex align-items-center">
-          <div style={{ width: "3rem", height: "auto" }}>
-            {/* <iframe
+    <Navbar
+      collapseOnSelect
+      expand="md"
+      variant="dark"
+      className="bg-success px-4 justify-content-between"
+    >
+      <div className="d-flex flex-row align-items-center">
+        <div style={{ width: "3rem", height: "auto" }}>
+          {/* <iframe
             src={"https://giphy.com/embed/EGzDfFUauCo6nDgfpk"}
             width="100"
             height="70"
             className="giphy-embed ms-1"
             allowFullScreen
           ></iframe> */}
-            <Image src={"/vegetables.jpg" || ""} fluid />
-          </div>
+          <Image src={"/vegetables.jpg" || ""} fluid />
+        </div>
 
-          <h1 className="text-white ms-3 mb-0">What Can I Grow Now?</h1>
-        </div>
-        <div className="d-flex gap-4 align-items-center">
-          <NavLink to="/" className="custom-nav-link">
+        <Navbar.Brand className="text-white fs-2 ms-3 mb-0">
+          What Can I Grow Now?
+        </Navbar.Brand>
+      </div>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav"></Navbar.Toggle>
+      <Navbar.Collapse id="responsive-navbar-nav" className="ms-auto">
+        <Nav className="ms-auto align-items-center">
+          <Nav.Link as={NavLink} to="/" className="custom-nav-link">
             Home
-          </NavLink>
-          <NavLink to="/about" className="custom-nav-link">
+          </Nav.Link>
+
+          <Nav.Link as={NavLink} to="/about" className="custom-nav-link">
             About
-          </NavLink>
-          <NavLink to="/resources" className="custom-nav-link">
+          </Nav.Link>
+
+          <Nav.Link as={NavLink} to="/resources" className="custom-nav-link">
             Resources
-          </NavLink>
-        </div>
-      </Navbar>
-    </header>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
