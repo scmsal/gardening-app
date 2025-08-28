62e8834d62efc5aa1e59f92181bdeea7ea96353e
scmsal <31stephcm@gmail.com>
2025-08-28 07:30:17 -0400

    edited conditional assignment of backend url

--- client/src/constants/backend.js
+++ client/src/constants/backend.js
@@ -1,6 +1,11 @@
 // Backend API URL with option for production (deployed on Render) or local development
 
+// const BACKEND_URL_ENDPOINT =
+//   import.meta.env.VITE_BACKEND_URL || "http://localhost:5050/api";
+
 const BACKEND_URL_ENDPOINT =
-  import.meta.env.VITE_BACKEND_URL || "https://localhost:5050/api";
+  process.env.NODE_ENV === "production"
+    ? process.env.VITE_BACKEND_URL
+    : "http://localhost:5050/api";
 
 export { BACKEND_URL_ENDPOINT };
