7b9b6f544f5f993bb7443b1b4b8f59a07a37c4fb
scmsal <31stephcm@gmail.com>
2025-08-28 07:31:53 -0400

    removed old App.jsx and renamed App1.jsx to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,67 @@
-import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import VeggiesList from "./components/VeggiesList";
-import PlantFetcher from "./components/PlantFetcher";
-import LinksToResources from "./components/LinksToResources";
-import { Row, Col } from "react-bootstrap";
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-import { useDispatch } from "react-redux";
-import FindZoneByZip from "./components/FindZoneByZip";
+const Header = lazy(() => import("./components/Header"));
+const HomePage = lazy(() => import("./pages/HomePage"));
+const ResourcesPage = lazy(() => import("./pages/Resources"));
+const AboutPage = lazy(() => import("./pages/About"));
+const PlantDetailPage = lazy(() => import("./pages/PlantDetailPage"));
+// const OrnamentalPlantsPage = lazy(() => import("./pages/ornamental"));
+const Footer = lazy(() => import("./components/Footer"));
 
+//Main app, my own implementation
 function App() {
-  //to clear cache for testing
-  //localStorage.removeItem("enrichedPlantData");
-
   return (
-    <div className="d-flex flex-column min-vh-100">
-      <Header />
-      <main className="flex-grow-1 pt-3">
-        <Row className="px-4">
-          <Col className="col-sm-12 col-md-6 col-lg-4">
-            <VeggiesList />
-          </Col>
-          <Col className="col-sm-12 col-md-6 col-lg-4">
-            <PlantFetcher />
-          </Col>
-          <Col className="col-sm-12 col-md-6 col-lg-4">
-            <FindZoneByZip />
-
-            {/* <DisplayGroup /> */}
-            <LinksToResources />
-          </Col>
-        </Row>
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="d-flex flex-column min-vh-100">
+        <Header />
+        <main className="flex-grow-1 px-5 pt-3">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <HomePage />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/about"
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <AboutPage />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/resources"
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <ResourcesPage />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/plants/:plantName"
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <PlantDetailPage />
+                </Suspense>
+              }
+            />
+            {/* <Route
+            path="/ornamental"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <OrnamentalPlantsPage />
+              </Suspense>
+            }
+          /> */}
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App1 from "./App1.jsx";
+import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store.js";
 import "./App.css";
@@ -9,7 +9,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App1 />
+      <App />
     </Provider>
   </React.StrictMode>
 );
