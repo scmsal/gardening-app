bd4040359b8b183a47b79831319a2a505f3e6eb0
scmsal <31stephcm@gmail.com>
2025-09-01 06:52:26 -0400

    restructured and centered footer text

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,33 +4,7 @@ import { Container, Row, Col } from "react-bootstrap";
 const Footer = () => {
   return (
     <footer className="bg-success bg-opacity-10 py-3 w-100 mt-auto">
-      <Container fluid className="ms-3">
-        <Row>
-          <Col>Copyright &copy; 2025</Col>
-          {/* <Col>
-            <APICallCounter />
-          </Col> */}
-          {/* <Col className="text-center">
-            Plant Data: {"  "}
-            <a
-              href="https://perenual.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              // className="ps-5"
-            >
-              Perenual.com
-            </a>
-          </Col> */}
-
-          {/* <Col className="text-center">
-            GIF via{"  "}
-            <a href="https://giphy.com/gifs/tomato-tomates-potager-EGzDfFUauCo6nDgfpk">
-              GIPHY
-            </a>
-          </Col> */}
-          <Col className="text-center"></Col>
-        </Row>
-      </Container>
+      <p className="text-center mb-0">Copyright &copy; 2025 </p>
     </footer>
   );
 };
