3b7a597dc39d66648bf96e71e6e54d74f0d413b0
scmsal <31stephcm@gmail.com>
2025-09-02 09:17:33 -0400

    added route for server welcome message; commented out unused allFoodPlants route

--- server/app/routes/foodPlantRouter.js
+++ server/app/routes/foodPlantRouter.js
@@ -9,19 +9,21 @@ import {
   fetchAllHerbs,
   fetchAllVegetables,
   fetchNamesImgs,
+  displayWelcome,
 } from "../services/foodPlantService.js";
 
 const foodPlantRouter = express.Router();
 
 //Routes
-foodPlantRouter.get(
-  "/allFoodPlants",
-  asyncHandler(async (req, res) => {
-    const data = await fetchAllFoodPlants();
-    console.log("foodPlantRouter/allFoodPlants hit");
-    res.status(200).json(data);
-  })
-);
+//This route is not currently in use until data cleaning is complete
+// foodPlantRouter.get(
+//   "/allFoodPlants",
+//   asyncHandler(async (req, res) => {
+//     const data = await fetchAllFoodPlants();
+//     console.log("foodPlantRouter/allFoodPlants hit");
+//     res.status(200).json(data);
+//   })
+// );
 
 foodPlantRouter.get(
   "/namesImgs",
@@ -61,6 +63,14 @@ foodPlantRouter.get(
   asyncHandler(async (req, res) => {
     const data = await fetchAllVegetables();
     res.status(200).json(data);
+  })
+);
+
+foodPlantRouter.get(
+  "/welcome",
+  asyncHandler(async (req, res) => {
+    const data = await displayWelcome();
+    res.status(200).send(data);
   })
 );
 export default foodPlantRouter;
--- server/app/services/foodPlantService.js
+++ server/app/services/foodPlantService.js
@@ -73,6 +73,42 @@ const fetchAllVegetables = async () => {
   return vegetables;
 };
 
+const displayWelcome = () => {
+  return `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Welcome</title>
+        <style>
+        body {
+        font-family: Arial, sans-serif;
+        }
+         h1 {
+        color: green;
+        }
+        a {
+        color: green}
+        </style>
+      </head>
+      <body>
+        <h1>Welcome to the Gardening App API</h1>
+        <p>Author: Stephanie Salifu</p>
+        <p>Explore available endpoints to find information about various edible plants you can grow in your own garden.</p>
+        <ul>
+          <li><a href="/api/allFoodPlants">Full Edible Plant List</a></li>
+          <li><a href="/api/listAllNames">Edible Plant Names</a></li>
+          <li><a href="/api/getFoodPlantByCommonName?common_name=Tomato">Get Plant by Common Name (e.g., Tomato)</a></li>
+          <li><a href="/api/namesImgs">Edible Plant Names and Images</a></li>
+          <li><a href="/api/herbs">Herbs</a></li>
+          <li><a href="/api/vegetables">Vegetables</a></li>
+        </ul>
+        <p>AI tools were used to generate the plant data. Image urls and credits were manually curated.</p>
+        <p>You can access the frontend at <a href="https://gardening-app-gules.vercel.app/">https://gardening-app-gules.vercel.app/</a></p>
+      </body>
+    </html>
+    `;
+};
+
 // Exporting the functions for use in routes
 
 export {
@@ -82,4 +118,5 @@ export {
   fetchFoodPlantByCommonName,
   fetchAllHerbs,
   fetchAllVegetables,
+  displayWelcome,
 };
